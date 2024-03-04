9efbf1fbc975f022c72778b90112402c04c2216d
Robert Grundy <R.A.Grundy@live.co.uk>
2024-03-04 20:51:26 +0000

    chore: taking the time to update main.go so that manual testing of the function injection could occur

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 
 	"github.com/simplesnake1/checkout-kata/internal/app/checkout"
+	"github.com/simplesnake1/checkout-kata/internal/app/pricing"
 )
 
 func main() {
-	c := checkout.NewCheckout(func(s string, i int) int { return -1 })
+	pl := map[string]pricing.Pricing{
+		"A": {UnitPrice: 50, SpecialPrice: 130, SpecialThreshold: 3},
+		"B": {UnitPrice: 30, SpecialPrice: 45, SpecialThreshold: 2},
+		"C": {UnitPrice: 20},
+		"D": {UnitPrice: 15},
+	}
+	calc := pricing.NewCalculator(pl)
+	c := checkout.NewCheckout(calc.GetPrice)
 	if c != nil {
 		fmt.Println("Checkout created.")
 	}
@@ -15,4 +23,6 @@ func main() {
 	c.Scan("B")
 	c.Scan("A")
 	c.Scan("B")
+
+	fmt.Println(c.GetTotalPrice())
 }

25a0fed96c6edcfc1bc2355feeb2053ce91984ab
Robert Grundy <R.A.Grundy@live.co.uk>
2024-03-04 19:53:45 +0000

    refactor: updating test to pass in checkout, as we've already tested that

--- internal/app/checkout/checkout_test.go
+++ internal/app/checkout/checkout_test.go
@@ -31,6 +31,7 @@ func TestCheckout_NewCheckout(t *testing.T) {
 func TestCheckout_Scan(t *testing.T) {
 	type Test struct {
 		name     string
+		checkout *checkout
 		scanned  []string
 		expected map[string]int
 	}
@@ -38,18 +39,53 @@ func TestCheckout_Scan(t *testing.T) {
 	tests := []Test{
 		{
 			name:     "Scan adds item to basket",
+			checkout: NewCheckout(),
 			scanned:  []string{"A"},
 			expected: map[string]int{"A": 1},
 		},
 		{
 			name:     "Scan adds item to basket, and increments count on subsequent calls with the same item",
+			checkout: NewCheckout(),
 			scanned:  []string{"A", "A"},
 			expected: map[string]int{"A": 2},
 		},
 		{
 			name:     "Scan adds item to basket, and creates new Key Value Pair on subsequent calls with different items",
+			checkout: NewCheckout(),
 			scanned:  []string{"A", "B"},
 			expected: map[string]int{"A": 1, "B": 1},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			for _, item := range test.scanned {
+				test.checkout.Scan(item)
+			}
+
+			for item, count := range test.expected {
+				if test.checkout.basket[item] != count {
+					t.Fatalf("Basket should contain %d for %s", count, item)
+				}
+			}
+		})
+
+	}
+}
+
+func TestCheckout_GetTotalPrice(t *testing.T) {
+	type Test struct {
+		name     string
+		scanned  []string
+		expected int
+	}
+
+	tests := []Test{
+		{
+			name:     "GetTotalPrice returns 0 when basket does not contain any item with valid sku",
+			scanned:  []string{"E"},
+			expected: 0,
 		},
 	}
 
@@ -60,13 +96,6 @@ func TestCheckout_Scan(t *testing.T) {
 			for _, item := range test.scanned {
 				c.Scan(item)
 			}
-
-			for item, count := range test.expected {
-				if c.basket[item] != count {
-					t.Fatalf("Basket should contain %d for %s", count, item)
-				}
-			}
 		})
-
 	}
 }
